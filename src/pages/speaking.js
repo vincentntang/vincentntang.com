9b1603e8e6332b23115515488bb63bfa9739711d
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2023-03-16 09:38:12 -0400

    update website and slides

--- src/pages/speaking.js
+++ src/pages/speaking.js
@@ -80,7 +80,8 @@ export const pageQuery = graphql`
             demo
             repo
             slides
-            site
+            website
+            audio
             video
           }
         }
